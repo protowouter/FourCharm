f68324ebf93bcf0d3552bbb7d44bcab175db186c
Wouter Timmermans <wouter@woutertimmermans.nl>
2014-11-15 13:20:25 +0100

    renamed board to connect4

---
+++ src/ft/model/Connect4.java
@@ -0,0 +1,34 @@
+/**
+ * Heavily influenced by the Fhourstones 3.0 Board Logic Copyright 2000-2004 John Tromp
+ * copyright 2014 Luce Sandfort and Wouter Timmermans 
+ */
+package ft.model;
+
+/**
+ * @author Luce Sandfort and Wouter Timmermans
+ *
+ */
+public class Connect4 {
+	
+	/**
+	 * Class for modeling an board for the game connect four. This class's responsibility is
+	 * to keep the state of the board 
+	 */
+	
+	
+	/**
+	 * Amount of columns of the board.
+	 */
+	public static final int COLUMNS = 7;
+	/**
+	 * Amount of rows of the board.
+	 */
+	public static final int ROWS	= 6;
+	
+	public Connect4() {
+		
+	}
+	
+	
+
+}
