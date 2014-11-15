edc5aa9b3bbbe3deca95436d5ea36615a52dfe38
Wouter Timmermans <wouter@woutertimmermans.nl>
2014-11-15 11:58:07 +0100

    Initial commit

---
+++ src/ft/model/Board.java
@@ -0,0 +1,16 @@
+/**
+ * copyright 2014 Luce Sandfort and Wouter Timmermans 
+ */
+package ft.model;
+
+/**
+ * @author Luce Sandfort and Wouter Timmermans
+ *
+ */
+public class Board {
+	
+	public Board() {
+		
+	}
+
+}
