47954bcce0f1a2062c194d2bb5db6ef913437891
Wouter Timmermans <wouter@woutertimmermans.nl>
2014-11-22 11:13:42 +0100

    added test suite

---
+++ src/ft/test/AllTests.java
@@ -0,0 +1,18 @@
+/**
+ * copyright 2014 Luce Sandfort and Wouter Timmermans 
+ */
+package ft.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author Luce Sandfort and Wouter Timmermans
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ ReferenceBoardTest.class })
+public class AllTests {
+
+}
---
+++ src/ft/test/ReferenceBoardTest.java
@@ -0,0 +1,88 @@
+/**
+ * copyright 2014 Luce Sandfort and Wouter Timmermans 
+ */
+package ft.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Luce Sandfort and Wouter Timmermans
+ *
+ */
+public class ReferenceBoardTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link ft.model.board.ReferenceBoard#ReferenceBoard()}.
+	 */
+	@Test
+	public void testReferenceBoard() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ft.model.board.ReferenceBoard#columnHasFreeSpace(int)}.
+	 */
+	@Test
+	public void testColumnHasFreeSpace() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ft.model.board.ReferenceBoard#lastMoveWon()}.
+	 */
+	@Test
+	public void testLastMoveWon() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ft.model.board.ReferenceBoard#full()}.
+	 */
+	@Test
+	public void testFull() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ft.model.board.ReferenceBoard#plieCount()}.
+	 */
+	@Test
+	public void testPlieCount() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ft.model.board.ReferenceBoard#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ft.model.board.ReferenceBoard#deepCopy()}.
+	 */
+	@Test
+	public void testDeepCopy() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link ft.model.board.ReferenceBoard#makemove(int)}.
+	 */
+	@Test
+	public void testMakemove() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
