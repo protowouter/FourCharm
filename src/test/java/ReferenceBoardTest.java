bd4fde259fabc62c5507187c4224f3666e3ef807
Wouter Timmermans <wouter@woutertimmermans.nl>
2014-11-22 12:46:21 +0100

    No failed tests

---
+++ src/test/java/ReferenceBoardTest.java
@@ -0,0 +1,88 @@
+/**
+ * copyright 2014 Luce Sandfort and Wouter Timmermans 
+ */
+package test.java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Luce Sandfort and Wouter Timmermans
+ *
+ */
+public class ReferenceBoardTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link main.java.model.board.ReferenceBoard#ReferenceBoard()}.
+	 */
+	@Test
+	public void testReferenceBoard() {
+		 // TODO
+	}
+
+	/**
+	 * Test method for {@link main.java.model.board.ReferenceBoard#columnHasFreeSpace(int)}.
+	 */
+	@Test
+	public void testColumnHasFreeSpace() {
+		 // TODO
+	}
+
+	/**
+	 * Test method for {@link main.java.model.board.ReferenceBoard#lastMoveWon()}.
+	 */
+	@Test
+	public void testLastMoveWon() {
+		 // TODO
+	}
+
+	/**
+	 * Test method for {@link main.java.model.board.ReferenceBoard#full()}.
+	 */
+	@Test
+	public void testFull() {
+		 // TODO
+	}
+
+	/**
+	 * Test method for {@link main.java.model.board.ReferenceBoard#plieCount()}.
+	 */
+	@Test
+	public void testPlieCount() {
+		 // TODO
+	}
+
+	/**
+	 * Test method for {@link main.java.model.board.ReferenceBoard#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		 // TODO
+	}
+
+	/**
+	 * Test method for {@link main.java.model.board.ReferenceBoard#deepCopy()}.
+	 */
+	@Test
+	public void testDeepCopy() {
+		 // TODO
+	}
+
+	/**
+	 * Test method for {@link main.java.model.board.ReferenceBoard#makemove(int)}.
+	 */
+	@Test
+	public void testMakemove() {
+		// TODO
+	}
+
+}
