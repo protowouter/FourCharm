869f8e7c778148341c38762cab7fd96dbfc0b40b
Wouter Timmermans <wouter@woutertimmermans.nl>
2014-11-23 15:32:02 +0100

    Red

---
+++ src/test/java/com/lucwo/fourcharm/features/BoardStateSteps.java
@@ -0,0 +1,38 @@
+/**
+ * copyright 2014 Luce Sandfort and Wouter Timmermans 
+ */
+package com.lucwo.fourcharm.features;
+
+
+import com.lucwo.fourcharm.model.board.Board;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import static org.junit.Assert.*;
+
+/**
+ * @author Luce Sandfort and Wouter Timmermans
+ *
+ */
+public class BoardStateSteps {
+    
+    private static String TODO = "Not yet implemented";
+    
+    private Board board;
+    
+    @Given("^an board with only one free spot$")
+    public void an_board_with_only_one_free_spot() {
+        fail(TODO);
+    }
+    @When("^I fill the last spot$")
+    public void I_fill_the_last_spot() {
+        fail(TODO);
+    }
+    @Then("^the board should report its when asked$")
+    public void the_board_should_report_its_when_asked() {
+        fail(TODO);
+    }
+
+
+}
