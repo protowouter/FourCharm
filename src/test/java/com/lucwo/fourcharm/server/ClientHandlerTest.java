3486d72b7b0fda4e61637917fd0c8a18fc74aac1
Luce <lucesand@hotmail.com>
2015-01-20 09:16:03 +0100

    Tests aangemaakt

---
+++ src/test/java/com/lucwo/fourcharm/server/ClientHandlerTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2015. Luce Sandfort and Wouter Timmermans
+ */
+
+package com.lucwo.fourcharm.server;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClientHandlerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetName() throws Exception {
+
+    }
+
+    @Test
+    public void testSetName() throws Exception {
+
+    }
+
+    @Test
+    public void testGetClient() throws Exception {
+
+    }
+
+    @Test
+    public void testGetClientGroup() throws Exception {
+
+    }
+
+    @Test
+    public void testSetClientGroup() throws Exception {
+
+    }
+
+    @Test
+    public void testJoin() throws Exception {
+
+    }
+
+    @Test
+    public void testReady() throws Exception {
+
+    }
+
+    @Test
+    public void testDoMove() throws Exception {
+
+    }
+
+    @Test
+    public void testError() throws Exception {
+
+    }
+}
